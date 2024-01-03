41373141301a7ee080cb722778879bad8c40d36f
UggFR <93045942+UggFR@users.noreply.github.com>
2024-01-03 14:08:46 +0100

    Delete Front/covoit-viveris/src directory

--- Front/covoit-viveris/src/components/App.js
+++
@@ -1,8 +0,0 @@
-import Banner from './Banner.js'
-import CarShareList from './CarShareList.js'
-
-function App() {
-  return (<div><Banner title="CovoitViveris"/><CarShareList/></div>)
-}
-
-export default App;
