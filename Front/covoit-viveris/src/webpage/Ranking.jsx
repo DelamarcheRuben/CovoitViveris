e4603baf0ce27c0923718e42c16380a41cf245e0
unknown <rub.del@hotmail.fr>
2024-01-07 20:49:01 +0100

    update level up in database

--- Front/covoit-viveris/src/webpage/Ranking.jsx
+++ Front/covoit-viveris/src/webpage/Ranking.jsx
@@ -3,47 +3,52 @@ import { useState, useEffect } from 'react';
 import { Navbar }      from "../components/header/Navbar";
 import { ProfileView } from "../components/ProfileView";
 
-const users = [{ UID: 1, pseudo: "Esteban",   job: "Dev Full-Stack",   city: "Boulogne-Billancourt", nb_carshares: 549, kilometers: 8754, experience: 109, picture_background: "mountain" },
-               { UID: 2, pseudo: "Alexandra", job: "Business Analyst", city: "Montpellier",          nb_carshares: 93,  kilometers: 2750, experience: 47,  picture_background: "mountain" },
-               { UID: 3, pseudo: "Enzo",      job: "Developper",        city: "Nice",                nb_carshares: 80,  kilometers: 1800, experience: 35,  picture_background: "sky"      },
-               { UID: 4, pseudo: "Dimitri",   job: "IT Manager",       city: "Boulogne-Billancourt", nb_carshares: 58,  kilometers: 1500, experience: 33,  picture_background: "forest"   },
-               { UID: 5, pseudo: "Solenne",   job: "UX Designer",      city: "Montpellier",          nb_carshares: 43,  kilometers: 1240, experience: 28,  picture_background: "land"     },
-               { UID: 6, pseudo: "Yann",      job: "RH",               city: "Montpellier",          nb_carshares: 39,  kilometers: 1150, experience: 6,  picture_background: "desert"   }
-]
-
-// var users = [];
-// async function getUsers() {
-//   const response = await fetch("http://localhost:8080/users");
-//   users = await response.json();
-// }
-// await getUsers();
+// const users = [{ UID: 1, pseudo: "Esteban",   job: "Dev Full-Stack",   city: "Boulogne-Billancourt", nb_carshares: 549, kilometers: 8754, experience: 109, picture_background: "mountain" },
+//                { UID: 2, pseudo: "Alexandra", job: "Business Analyst", city: "Montpellier",          nb_carshares: 93,  kilometers: 2750, experience: 47,  picture_background: "mountain" },
+//                { UID: 3, pseudo: "Enzo",      job: "Developper",        city: "Nice",                nb_carshares: 80,  kilometers: 1800, experience: 35,  picture_background: "sky"      },
+//                { UID: 4, pseudo: "Dimitri",   job: "IT Manager",       city: "Boulogne-Billancourt", nb_carshares: 58,  kilometers: 1500, experience: 33,  picture_background: "forest"   },
+//                { UID: 5, pseudo: "Solenne",   job: "UX Designer",      city: "Montpellier",          nb_carshares: 43,  kilometers: 1240, experience: 28,  picture_background: "land"     },
+//                { UID: 6, pseudo: "Yann",      job: "RH",               city: "Montpellier",          nb_carshares: 39,  kilometers: 1150, experience: 6,  picture_background: "desert"   }
+// ]
 
 const Ranking = () => {
 
-  //const [usersList, setUsersList] = useState([]); //initiate usersList to empty
-  const [usersList, setUsersList] = useState(users);
+  const [usersList, setUsersList] = useState([]); //initiate usersList to empty
+  //const [usersList, setUsersList] = useState(users);
 
-  
-  // useEffect(() => {
-  //   fetch("http://localhost:8080/users")
-  //     .then((res) => {
-  //       return res.json();
-  //     })
-  //     .then((data) => {
-  //       console.log(data);
-  //       setUsersList(data);
-  //     });
-  // }, []);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/users")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setUsersList(data);
+      });
+  }, []);
 
   const addLevel = () => {
+    const uid_update = 4;
     const newList = usersList.map(item => {
 
-      if (item.UID === 4) {
-        return { ...item, experience: item.experience+1 };
+      if (item.UID === uid_update) {
+        const data = { ...item, experience: item.experience+1 };
+        const options = {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(data)
+        };
+        fetch("http://localhost:8080/user/"+uid_update, options)
+        .then((res) => {
+        })
+        return data;
       }
       return item;
     });
-    console.log(newList);
+    
 
     // Update the users list
     setUsersList(newList);
