9fc06f73092e5eb5d6c4c91b7504b9f7e8e12e1d
unknown <rub.del@hotmail.fr>
2024-01-15 12:16:06 +0100

    maj schema database

--- Script_BDD/script_insert_users.sql
+++ Script_BDD/script_insert_users.sql
@@ -5,3 +5,6 @@ INSERT INTO users (pseudo, job, city, picture_background, nb_carshares, kilomete
 (  "Dimitri",    "IT Manager",        "Boulogne-Billancourt", "sky", 58,   1500,  33),
 (  "Solenne",    "UX Designer",       "Montpellier", "mountain" ,  43,   1240,  28),
 (  "Yann",       "RH",                "Montpellier", "mountain" ,  39,   1150,  6)
+
+INSERT INTO users (pseudo, job, city, picture_background, nb_carshares, kilometers, experience) VALUES 
+("Ruben",    "Dev Back",    "Paris",    "desert"   ,10,  50,  8);
