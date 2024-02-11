0f3ced213693d67527870af7ea6fb583b91cd2f2
unknown <rub.del@hotmail.fr>
2024-02-11 23:22:13 +0100

    update not-full-carshare

--- Back/API/src/main/java/com/viveris/api/model/Company.java
+++ Back/API/src/main/java/com/viveris/api/model/Company.java
@@ -3,6 +3,8 @@ package com.viveris.api.model;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
@@ -13,6 +15,7 @@ import lombok.Data;
 public class Company {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="UID")
     public Long uid;
     
