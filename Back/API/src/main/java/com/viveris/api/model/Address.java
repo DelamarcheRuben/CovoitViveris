3166cbced0e4286d970e1d16b4d1c21d43c415c2
UggFR <93045942+UggFR@users.noreply.github.com>
2024-02-14 16:26:44 +0100

    Ajout post adresse

--- Back/API/src/main/java/com/viveris/api/model/Address.java
+++ Back/API/src/main/java/com/viveris/api/model/Address.java
@@ -8,6 +8,8 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -21,6 +23,7 @@ import lombok.Getter;
 public class Address {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="UID")
     public Long uid;
     
