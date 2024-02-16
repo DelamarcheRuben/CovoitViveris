1ac0bcc5572441ff2b9ae6f89db58561007c3751
unknown <rub.del@hotmail.fr>
2024-02-16 22:06:36 +0100

    creation adresses quand creation carshares

--- Back/API/src/main/java/com/viveris/api/repository/AddressRepository.java
+++ Back/API/src/main/java/com/viveris/api/repository/AddressRepository.java
@@ -1,9 +1,17 @@
 package com.viveris.api.repository;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.viveris.api.model.Address;
 
 @Repository
 public interface AddressRepository extends CrudRepository<Address, Long> {
+	@Query(value = "SELECT * from Address WHERE city = :city AND department = :department AND postcode = :postcode"
+			+ " AND road = :road AND house_number = :house_number", nativeQuery = true)
+	Optional<Address> findAddress(@Param("city") String city, @Param("department") String department,
+			@Param("postcode") Integer postcode, @Param("road") String road, @Param("house_number") Integer house_number);
 }
--- Back/API/src/main/java/com/viveris/api/service/CarshareService.java
+++ Back/API/src/main/java/com/viveris/api/service/CarshareService.java
@@ -5,7 +5,9 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.viveris.api.model.Address;
 import com.viveris.api.model.Carshare;
+import com.viveris.api.repository.AddressRepository;
 import com.viveris.api.repository.CarshareRepository;
 
 import lombok.Data;
@@ -16,6 +18,9 @@ public class CarshareService {
 
 	@Autowired
 	private CarshareRepository carshareRepository;
+	
+	@Autowired
+	private AddressRepository addressRepository;
 	
 	public Optional<Carshare> getCarshare(final Long id) {
 		return carshareRepository.findById(id);
@@ -31,6 +36,35 @@ public class CarshareService {
 	}
 	
 	public Carshare saveCarshare(Carshare Carshare) {
+		Address start_place = Carshare.getStart_place();
+		Address end_place = Carshare.getEnd_place();
+		
+		Optional<Address> address = addressRepository.findAddress(start_place.city, start_place.department, 
+				start_place.postcode, start_place.road, start_place.house_number);
+		if(address.isPresent())
+		{
+			start_place = address.get();
+		}
+		else
+		{
+			start_place = addressRepository.save(start_place);
+		}
+		
+		address = addressRepository.findAddress(start_place.city, end_place.department, 
+				end_place.postcode, end_place.road, end_place.house_number);
+		if(address.isPresent())
+		{
+			end_place = address.get();
+		}
+		else
+		{
+			end_place = addressRepository.save(end_place);
+		}
+		
+		
+		Carshare.setStart_place(start_place);
+		Carshare.setEnd_place(end_place);
+		
 		Carshare savedCarshare = carshareRepository.save(Carshare);
 		return savedCarshare;
 	}
