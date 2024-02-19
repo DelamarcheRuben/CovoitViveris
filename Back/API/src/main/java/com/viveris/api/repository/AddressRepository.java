02202a760ec114540074d9ecbca6544d358498ef
unknown <rub.del@hotmail.fr>
2024-02-19 22:58:12 +0100

    fix bug doublon address

--- Back/API/src/main/java/com/viveris/api/repository/AddressRepository.java
+++ Back/API/src/main/java/com/viveris/api/repository/AddressRepository.java
@@ -10,8 +10,12 @@ import com.viveris.api.model.Address;
 
 @Repository
 public interface AddressRepository extends CrudRepository<Address, Long> {
-	@Query(value = "SELECT * from Address WHERE city = :city AND department = :department AND postcode = :postcode"
-			+ " AND road = :road AND house_number = :house_number", nativeQuery = true)
+	@Query(value = "SELECT * from Address WHERE (city = :city OR :city IS NULL) "
+			+ "AND (department = :department OR :department IS NULL) "
+			+ "AND (postcode = :postcode OR :postcode IS NULL) "
+			+ "AND (road = :road OR :road IS NULL) "
+			+ "AND (house_number = :house_number OR :house_number IS NULL)"
+			+ " LIMIT 1", nativeQuery = true)
 	Optional<Address> findAddress(@Param("city") String city, @Param("department") String department,
 			@Param("postcode") Integer postcode, @Param("road") String road, @Param("house_number") Integer house_number);
 }
