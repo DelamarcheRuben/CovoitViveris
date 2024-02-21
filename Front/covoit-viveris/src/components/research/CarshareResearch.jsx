dfe68463391e0ec457478340b5cee63ffa1b7a2a
unknown <rub.del@hotmail.fr>
2024-02-21 22:25:58 +0100

    no change i hope

--- Front/covoit-viveris/src/components/research/CarshareResearch.jsx
+++ Front/covoit-viveris/src/components/research/CarshareResearch.jsx
@@ -1,15 +1,17 @@
 import React, { useState } from "react";
+import { useUser }        from "../../context/UserContext.jsx";
 import CarshareResearchForm from "./CarshareResearchForm";
 import CarshareResearchResultsDisplay from "./CarshareResearchResultDisplay.jsx";
 
 const CarshareResearch = () => {
     const [searchResults, setSearchResults] = useState([]);
+    const { user } = useUser();
 
     const handleSearchResults = (results) => {
         setSearchResults(results);
     };
 
-    return (
+    return (user &&
         <div>
             <CarshareResearchForm onSearchResults={handleSearchResults} />
             {searchResults.length > 0 && <CarshareResearchResultsDisplay searchResults={searchResults} />}
